e89f292bfd7dd0dc2ca2e8b429215204cfbba39b
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-02-24 11:13:22 -0600

    updated db model

---
+++ sqlite/db_model.py
@@ -0,0 +1,10 @@
+from userdb import Base
+from sqlalchemy import Column, Integer, String
+
+class User_Table(Base):   #class to create schema for the table of users in db
+    __tablename__ = "users"  
+    
+    id = Column(Integer, primary_key= True, index=True)
+    name = Column(String)
+    username = Column(String)
+    password = Column(String)
